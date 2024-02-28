26465fcb8dafc1bd968f4e5a0e7f5fb647407ea6
Vlados <vladislav.mocilenko@student.tuke.sk>
2024-02-28 15:32:03 +0100

    removed useless color value variables from tileinfo

--- game/src/bricksbreaker/core/TileInfo.java
+++ game/src/bricksbreaker/core/TileInfo.java
@@ -5,23 +5,17 @@ package bricksbreaker.core;
 //tcolor = Text color
 
 public enum TileInfo {
-    RED(255,0,0, 20, 'R', "\u001B[41m" ,"\u001B[30m"),
-    GREEN(0,255,0 ,10, 'G',"\u001B[42m" ,"\u001B[30m"),
-    BLUE(0,0,255 ,5, 'B', "\u001B[44m", "\u001B[30m"),
-    NONE(0,0,0,0,' ', "\u001B[40m", "\u001B[37m");
+    RED(20, 'R', "\u001B[41m" ,"\u001B[30m"),
+    GREEN(10, 'G',"\u001B[42m" ,"\u001B[30m"),
+    BLUE(5, 'B', "\u001B[44m", "\u001B[30m"),
+    NONE(0,' ', "\u001B[40m", "\u001B[37m");
 
-    private final int r;
-    private final int g;
-    private final int b;
     private final int score;
     private final char sname;
     private final String bcolor;
     private final String tcolor;
 
-    TileInfo(int r, int g, int b, int score, char sname, String bcolor, String tcolor) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
+    TileInfo(int score, char sname, String bcolor, String tcolor) {
         this.score = score;
         this.sname = sname;
         this.bcolor = bcolor;
